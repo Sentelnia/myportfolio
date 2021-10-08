00757749ff6577029f32e330f0c63d0c16d51ed9
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-10-08 16:49:25 +0200

    animintro

--- client/src/components/intro/Intro.js
+++ client/src/components/intro/Intro.js
@@ -3,6 +3,7 @@ import "./Intro.css";
 import { animLetter } from "./anime_intro"
 import lampionRond from "../../images/LAMPION_ARRONDI_SVG.svg"
 import lampionLong from "../../images/LAMPION_ALLONGE_SVG.svg"
+import sakura from "../../images/SAKURA_SVG.svg"
 
 class Intro extends Component{
 
@@ -22,8 +23,16 @@ class Intro extends Component{
       <>
         {this.props.show && 
         <div className='show' onWheel={(e) => this.onClose(e)}>
-          <img className="lampionRond" src ={lampionRond} alt="Lampion_rond"/>
-          <img className="lampionLong" src ={lampionLong} alt="Lampion_long"/>
+          <div className="lampionRond">
+            <img src ={lampionRond} alt="Lampion_rond"/>
+            <div className="haloRond"></div> 
+          </div>
+          <div className="lampionLong">
+            <img src ={lampionLong} alt="Lampion_long"/>
+            <div className="haloLong"></div>
+          </div>
+          <div className="shadowLong"></div>
+          <div className="shadowRond"></div>
           <h1 className='introTittle'></h1>
           <button className='enter' onClick={this.onClose}><span className='letters1'></span></button>
         </div>} 
