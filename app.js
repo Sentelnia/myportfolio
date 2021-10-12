fbbfc6b31731b75f6eb8452e2138c102d1a6a960
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-10-12 17:43:41 +0200

    heroku

--- app.js
+++ app.js
@@ -68,4 +68,14 @@ const projectsRoutes = require('./routes/projects');
 app.use('/api', projectsRoutes);
 
 
+// Serve static files from client/build folder
+app.use(express.static('client/build'));
+
+// For any other routes: serve client/build/index.html SPA
+app.use((req, res, next) => {
+  res.sendFile(`${__dirname}/client/build/index.html`, err => {
+    if (err) next(err)
+  })
+});
+
 module.exports = app;
