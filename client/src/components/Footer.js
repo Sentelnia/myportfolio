752f2265206ef81d276cd8626c6db00a97e70142
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-10-15 19:15:29 +0200

    footer et navbar

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,40 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { ExternalLink } from "react-external-link";
-import "./Fotter.css"
+import "./Footer.css";
+import Branche from "../images/BRANCHE_CERISIER_LIGHT_SVG.svg";
+import Likedin from "../images/kisspng-linkedin-computer-icons-logo-advertising-community-5ac706ee380906.0987186415229928782295.png"
+import Github from "../images/25231.svg"
+import Codewars from "../images/codewars-pngrepo-com.png"
+import Sakura from "../images/SAKURA_SVG.svg"
 
 class Footer extends Component {
-  render(){
+  render() {
     return (
       <footer>
-        <div className='contactLink'>
-          <ul>
-            <li>
-              <ExternalLink href="">
-                ln
-              </ExternalLink>
-            </li>
-            <li>
-              <ExternalLink href="">
-                Git
-              </ExternalLink>
-            </li>
-            <li>
-              <ExternalLink href="">
-                CodeWars
-              </ExternalLink>
-            </li>
-          </ul>
-        </div>
-        <div className='credit'>
-        <ul>
-          <li> © Create & design by Mathilde Tomba</li>
-          <li> © Illustration by Tom PetitJean</li>
-        </ul>
+        <img
+          className="brancheFooter"
+          src={Branche}
+          alt="branche de cerisier"
+        />
+        <div className="infos">
+          <div className="contactLink">
+            <ul>
+              <li>
+                <ExternalLink href="https://www.linkedin.com/in/mathilde-tomba/"><img src={Likedin} alt="Icône Likedin"/></ExternalLink>
+              </li>
+              <li>
+                <ExternalLink href="https://github.com/Sentelnia"><img src={Github} alt="Icône Github"/></ExternalLink>
+              </li>
+              <li>
+                <ExternalLink href="https://www.codewars.com/users/Sentelnia"><img src={Codewars} alt="Icône Codewars"/></ExternalLink>
+              </li>
+            </ul>
+          </div>
+          <div className="credit">
+            <ul>
+              <li><img src={Sakura} alt="fleur de cerisier"/> © Create & design by Mathilde Tomba</li>
+              <li><img src={Sakura} alt="fleur de cerisier"/> © Illustration by Tom PetitJean</li>
+            </ul>
+          </div>
         </div>
       </footer>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
