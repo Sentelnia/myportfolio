33b986db7b3b2927f56fe9f6d3108890dc5f44fb
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-10-17 16:48:25 +0200

    v2 details project

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -8,15 +8,14 @@ import MediaQuery from "react-responsive";
 
 class Project extends Component {
   state = {
-    projects:[]
+    projects:[],
+    détails:{}
   }
-
 
 
   componentDidMount() {
     projectsServices.getProjects()
     .then(data => {
-      console.log('data',data)
       this.setState({
         projects: data.allProjectsFromDB
       })
@@ -27,19 +26,46 @@ class Project extends Component {
     });
   }
 
+  details = (e,id) =>{
+    e.preventDefault();
+
+    
+    projectsServices.getProject(id)
+    .then(details => {
+      console.log(details)
+      this.setState({
+        details: details
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    });
+    console.log(id)
+  }
+
   render(){
     return (
       <div className ="projects">
+      {/* Détails du projet cliqué */}
+      <div className='detailsproject'>
+      {this.state.details &&
+        <div>
+          <p>{this.state.details.title}</p>
+        </div>}
+      </div>
+      {/* La liste des projets */}
         <h1>Mes projets</h1>
         <div className="allProjects">
         {this.state.projects.map(project =>{
           return (
             <div className='project' key={project._id}>
-              <Link to={{ pathname : project.linkUrl}} target="_blank">
+              {/* <Link to={{ pathname : project.linkUrl}} target="_blank"> */}
+              
               {project.title === "L'Alcôve" ? 
-              <img src={project.imgUrl} className='gif' alt={`Miniature de ${project.title}`}/>
-              : <img src={project.imgUrl} className='miniature' alt={`Miniature de ${project.title}`}/>}
-              </Link>
+              <img src={project.imgUrl} className='gif' alt={`Miniature de ${project.title}`} onClick={(e) => this.details(e, project._id)}/>
+              : <img src={project.imgUrl} className='miniature' alt={`Miniature de ${project.title}`} onClick={(e) => this.details(e, project._id)}/>}    
+              
+              {/* </Link> */}
               <h2>{project.title}</h2>
               <p>{project.subtitle}</p>
               <MediaQuery orientation="portrait">
@@ -56,6 +82,16 @@ class Project extends Component {
           )
         })}
         </div>
+        <MediaQuery orientation="landscape">
+                <div className="interlude">
+                  <img src={Sakura} alt="fleur de cerisier"/>
+                  <img src={Sakura} alt="fleur de cerisier"/>
+                  <img src={Sakura} alt="fleur de cerisier"/>
+                  <img src={Sakura} alt="fleur de cerisier"/>
+                  <img src={Sakura} alt="fleur de cerisier"/>
+                  <img src={Sakura} alt="fleur de cerisier"/>
+                </div>
+        </MediaQuery>
       </div>
     )
   }
