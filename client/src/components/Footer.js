6c5cd26f1d65b4f861db11d0461950e6e99b3fdd
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-10-19 17:41:41 +0200

    contact

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -23,6 +23,12 @@ class Footer extends Component {
         </div>
         </MediaQuery>
         <div className="infos">
+          <div className="contact">
+            <ul>
+              <li><ExternalLink href="mailto:mathilde.tomba@gmail.com">Me contacter par mail : mathilde.tomba@gmail.com</ExternalLink></li>
+              <li><ExternalLink href="tel=:+33630196429">M'appeler : 0630196429</ExternalLink></li>
+            </ul>
+          </div>
           <div className="contactLink">
             <ul>
               <li>
